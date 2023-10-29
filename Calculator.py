b1263f4164019c8291c94b9360e49428a373c651
Pitonchik2012 <143886029+Pitonchik2012@users.noreply.github.com>
2023-10-29 09:34:00 +0700

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+while True:
+    print(eval(input('>>>')))
+#The eval() function executes
+#an expression string passed
+#to it as a mandatory argument 
+#and returns the result of
+#executing this string.
